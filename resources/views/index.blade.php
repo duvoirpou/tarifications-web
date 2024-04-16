a313d5c580bf2bd2ef3c8bb03d9049a1dfb5dc6b
Précieux Assako <assakoprecieux19@gmail.com>
2024-04-16 23:58:37 +0200

    "Update index.blade.php: French translation and service description updates"
    This commit updates the index.blade.php file with French translations for the page title and service descriptions. The Cost Calculator and Order Wizard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
         <!-- Hero -->
         <div class="hero-home bg-mockup">
             <div class="content">
-                <h3>Cost Calculator</h3>
-                <p>& Order Wizard. With PDF Summary generation.</p>
+                <h3>Calculateur de coûts</h3>
+                <p>& Assistant de commande.  Avec génération de résumé PDF.</p>
                 <a href="{{ route('tarifications') }}" class="btn-1 medium">Demandez un dévis</a>
             </div>
         </div>
@@ -37,8 +37,8 @@
                                 <div class="box text-center">
                                     <div class="icon d-flex align-items-end"><i class="icon icon-calculator-1 color-1"></i>
                                     </div>
-                                    <h3 class="service-title">Cost Calculations</h3>
-                                    <p>One of your services which tells about price calculations.</p>
+                                    <h3 class="service-title"> Calcul précis des coûts</h3>
+                                    <p>Génère des estimations détaillées et précises des coûts pour divers projets web.</p>
                                 </div>
                             </a>
                         </div>
@@ -47,8 +47,8 @@
                                 <div class="box text-center">
                                     <div class="icon d-flex align-items-end"><i
                                             class="icon icon-shopping-cart-1 color-2"></i></div>
-                                    <h3 class="service-title">Order Wizard</h3>
-                                    <p>One of your services which tells about order generation.</p>
+                                    <h3 class="service-title">Assistant de commande</h3>
+                                    <p>Génération d'un pdf de la commandes illustrant tous les détails du projet web défini.</p>
                                 </div>
                             </a>
                         </div>
@@ -56,17 +56,20 @@
                             <a href="faq.html" class="service-link">
                                 <div class="box text-center">
                                     <div class="icon d-flex align-items-end"><i class="icon icon-email color-3"></i></div>
-                                    <h3 class="service-title">Email Summary</h3>
-                                    <p>One of your services which tells about auto email summary.</p>
+                                    <h3 class="service-title">Résumé par e-mail</h3>
+                                    <p>Bref aperçu des points clés du projet web dans un document envoyé par email.</p>
                                 </div>
                             </a>
                         </div>
                         <div class="col-lg-4">
                             <a href="faq.html" class="service-link">
                                 <div class="box text-center">
-                                    <div class="icon d-flex align-items-end"><i class="icon icon-truck color-3"></i></div>
-                                    <h3 class="service-title">Quick Delivery</h3>
-                                    <p>One of your services which tells about safe and quick delivery.</p>
+                                    <div class="icon d-flex align-items-end">
+                                        {{-- <i class="icon icon-truck color-3"></i> --}}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M11 17H4a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3h16a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3h-7v2h3a1 1 0 1 1 0 2H8a1 1 0 1 1 0-2h3zM4 5h16a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1" clip-rule="evenodd"/></svg>
+                                    </div>
+                                    <h3 class="service-title">Facilité d'utilisation</h3>
+                                    <p>Interface intuitive et conviviale, même pour les utilisateurs non techniques.</p>
                                 </div>
                             </a>
                         </div>
@@ -74,8 +77,8 @@
                             <a href="faq.html" class="service-link">
                                 <div class="box text-center">
                                     <div class="icon d-flex align-items-end"><i class="icon icon-support color-1"></i></div>
-                                    <h3 class="service-title">Customer Support</h3>
-                                    <p>One of your services which tells about customer care.</p>
+                                    <h3 class="service-title">Gain de temps et d'argent</h3>
+                                    <p>Aide les utilisateurs à prendre des décisions éclairées concernant leurs investissements en matière de développement web.</p>
                                 </div>
                             </a>
                         </div>
